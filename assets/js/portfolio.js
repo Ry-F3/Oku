8dcedf8f1c6138beab625ddc90c22c7170e9c72d
Ry-F3 <rhysfeew@gmail.com>
2023-05-02 16:50:58 +0000

    Non fix - found issue with original elements are clicked to close, but no elements created will not close

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -7,29 +7,40 @@ addButton.forEach(div => div.addEventListener('click', incrementAsset));
 const inputNum = document.querySelectorAll("#assets-el");
 console.log(inputNum)
 
+const closeAsset = document.querySelectorAll("#close");
+closeAsset.forEach(div => div.addEventListener('click', close));
+console.log(closeAsset)
+
+
 function incrementAsset() {
 
     let addAsset = document.getElementById("add_asset");
     let asset = document.createElement("div");
     asset.innerHTML =
-        `   <select class= "assetBox" name="assetType" id="Type">
-                <option value="Crypto">&#xf379;</option>
-                <option value="Stock">&#xf201;</option>
-                <option value="commodities">&#xf3a5;</option>
-                <option value="bussiness">&#xf0b1;</option>
-                <option value="fiat">&#xf19c;</option>
-                <option value="real-estate">&#xe00d;</option>
-            </select>
-            <span>
-                <input class="pInput" type="text" pattern="[0-9.,]+" placeholder="$0" name="amount" id="assets-el" required data-type="number" />
-            </span> 
-            <div class="pSymbol">
-                <span>X</span>
-            </div>`;
-  
+    `   <select class= "assetBox" name="assetType" id="Type">
+            <option value="Crypto">&#xf379;</option>
+            <option value="Stock">&#xf201;</option>
+            <option value="commodities">&#xf3a5;</option>
+            <option value="bussiness">&#xf0b1;</option>
+            <option value="fiat">&#xf19c;</option>
+            <option value="real-estate">&#xe00d;</option>
+        </select>
+        <span>
+            <input class="pInput" type="number" pattern="[0-9.,]+" placeholder="$0" name="amount" id="assets-el"/>
+        </span> 
+        <div id="close" class="pSymbol">
+            <span>X</span>
+        </div>`;
+
     asset.classList.add("input_box", "inputBorder");
     addAsset.appendChild(asset);
     console.log(asset);
     console.log(addAsset);
 
+};
+
+function close() {
+    
+    let remove =  document.getElementsByClassName("input_box", "inputBorder")[3].remove(); 
+    
 };
