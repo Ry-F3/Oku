238602c78faab81a186caddb7186152e61358b8a
Ry-F3 <rhysfeew@gmail.com>
2023-05-04 14:19:52 +0000

    Fix - portfolio section now has independently recognised event listener clicks achieved with for loops and using the querySelectorAll method

--- assets/js/demo.js
+++ assets/js/demo.js
@@ -76,3 +76,272 @@ function valueReset() { // Reset values of calculator back to default
 // let label = capSwitch.innerHTML;
 // console.log(label);
 // capSwitch.innerHTML = "<label>Price</label>";
+
+
+
+
+////// code for adding button
+
+
+const addButton = document.querySelectorAll(".plus");
+
+
+addButton.forEach(div => div.addEventListener('click', incrementAsset));
+
+const inputNum = document.querySelectorAll("#assets-el");
+console.log(inputNum)
+
+const closeAsset = document.querySelectorAll(".close");
+closeAsset.forEach(div => div.addEventListener('click', close));
+console.log(closeAsset)
+
+
+
+function incrementAsset() {
+
+    let addAsset = document.getElementById("add_asset");
+    let asset = document.createElement("div");
+    asset.innerHTML =
+       `<select class= "assetBox" name="assetType" id="Type">
+            <option value="Crypto">&#xf379;</option>
+            <option value="Stock">&#xf201;</option>
+            <option value="commodities">&#xf3a5;</option>
+            <option value="bussiness">&#xf0b1;</option>
+            <option value="fiat">&#xf19c;</option>
+            <option value="real-estate">&#xe00d;</option>
+        </select>
+        <span>
+            <input class="pInput" type="number" pattern="[0-9.,]+" placeholder="$0" name="amount" id="assets-el"/>
+        </span> 
+        <div class="pSymbol close">
+            <span onclick="close()">X</span>
+        </div>`;
+
+    asset.classList.add("input_box", "inputBorder");
+    addAsset.appendChild(asset);
+    console.log(asset);
+    console.log(addAsset);
+    
+};
+
+function close() {
+     
+    document.getElementsByClassName("input_box")[3].remove();
+      
+};
+
+
+// console.log(removeAsset);
+    // console.log(close);
+// let closeButton = document.getElementsByClassName("close");
+//         console.log(closeButton);
+var removeAsset =  document.getElementsByClassName("input_box", "inputBorder"); 
+for (var i = 0; i < removeAsset.length; i++) {
+removeAsset[i].addEventListener("click", close);
+ }
+
+// console.log(removeAsset)
+
+document.addEventListener('DOMContentLoaded', function(){
+    let container = document.getElementById('container')
+    
+    for (let i = 1; i < 10; i++){
+        container.innerHTML = `<div id=box-${i}></div>`
+    }
+
+    let button = document.getElementById('add-button')
+    button.addEventListener('click', function(){
+        container.innerHTML = '<div class="box">whatever</div>'
+    })
+
+    let boxes = this.getElementsByClassName('boxes')
+    // get an array
+    // [1, 2, 3, 4, 5, 6]
+
+    for (let i = 0; i < 10; i++){
+        i.setAttribute(`id="box-${i}`)
+    }
+    // removeAttribute(id)
+    
+        
+})
+
+
+
+// new portfolio code 
+
+document.addEventListener('DOMContentLoaded', function(){
+    let addAsset = document.getElementById("asset");
+ 
+    for(let i = 0; i < 10; i++){
+        addAsset.innerHTML = `  
+            <label class="input_label" for="asset-el">Assets</label>    
+            <div id="asset-${i}" class="input_box inputBorder removeAsset">
+                <select class= "assetBox" name="assetType" id="Type">
+                    <option value="Crypto">&#xf379;</option>
+                    <option value="Stock">&#xf201;</option>
+                    <option value="commodities">&#xf3a5;</option>
+                    <option value="bussiness">&#xf0b1;</option>
+                    <option value="fiat">&#xf19c;</option>
+                    <option value="real-estate">&#xe00d;</option>
+                </select>
+                <span>
+                    <input class="pInput" type="number" pattern="[0-9.,]+" placeholder="$0" name="amount" id="assets-el"/>
+                </span> 
+                <div class="pSymbol close">
+                    <span>X</span>
+                </div>
+            </div>
+        <div id="asset-${i}" class="input_box inputBorder removeAsset">
+            <select class= "assetBox" name="assetType" id="Type">
+                <option value="Crypto">&#xf379;</option>
+                <option value="Stock">&#xf201;</option>
+                <option value="commodities">&#xf3a5;</option>
+                <option value="bussiness">&#xf0b1;</option>
+                <option value="fiat">&#xf19c;</option>
+                <option value="real-estate">&#xe00d;</option>
+            </select>
+            <span>
+                <input class="pInput" type="number" pattern="[0-9.,]+" placeholder="$0" name="amount" id="assets-el"/>
+            </span> 
+            <div class="pSymbol close">
+                <span>X</span>
+            </div>
+        </div>
+        <div id="asset-${i}" class="input_box inputBorder removeAsset">
+            <select class= "assetBox" name="assetType" id="Type">
+                <option value="Crypto">&#xf379;</option>
+                <option value="Stock">&#xf201;</option>
+                <option value="commodities">&#xf3a5;</option>
+                <option value="bussiness">&#xf0b1;</option>
+                <option value="fiat">&#xf19c;</option>
+                <option value="real-estate">&#xe00d;</option>
+            </select>
+            <span>
+                <input class="pInput" type="number" pattern="[0-9.,]+" placeholder="$0" name="amount" id="assets-el"/>
+            </span> 
+            <div class="pSymbol close">
+                <span>X</span>
+            </div>
+        </div>`;
+    };
+
+  
+
+    let addButton = document.getElementById("plus_btn");
+    addButton.addEventListener('click', function(){
+        console.log("click add")
+        let addAsset = document.getElementById("asset");
+        let asset = document.createElement("div");
+        
+        asset.innerHTML =
+        ` 
+           <select class= "assetBox" name="assetType" id="Type">
+               <option value="Crypto">&#xf379;</option>
+               <option value="Stock">&#xf201;</option>
+               <option value="commodities">&#xf3a5;</option>
+               <option value="bussiness">&#xf0b1;</option>
+               <option value="fiat">&#xf19c;</option>
+               <option value="real-estate">&#xe00d;</option>
+           </select>
+           <span>
+               <input class="pInput" type="number" pattern="[0-9.,]+" placeholder="$0" name="amount" id="assets-el"/>
+           </span> 
+           `;
+    
+        asset.classList.add("input_box", "inputBorder", "removeAsset");
+        addAsset.appendChild(asset);
+        console.log(asset);
+        console.log(addAsset);
+        console.log(closeButton);
+
+        let add = document.getElementById("asset-9");
+
+        let closeBtn = document.createElement("div");
+        closeBtn.addEventListener('click', function handleClick(event) {
+            close();
+        });
+
+        closeBtn.innerHTML = `<span>X</span>`;
+        closeBtn.classList.add("pSymbol", "close");
+        asset.appendChild(closeBtn);
+
+    
+
+        for (let i = 0; i < 10; i++){
+            asset.setAttribute("id", `asset-${i}`);
+        };
+
+
+        
+    });
+
+ 
+
+
+    let assetNum = document.getElementById("asset-9");
+    console.log(assetNum);
+   
+
+    // for (let i = 0; i < 10; i++){
+    //     i.setAttribute[0]= (`id="asset-${i}`);
+    // };
+
+    // console.log(i);
+
+    
+    // const closeAsset = document.querySelectorAll(".close");
+    // closeAsset.forEach(div => div.addEventListener('click', close));
+
+    let closeButton = this.getElementsByClassName("close");
+    console.log(closeButton);
+    let i = 0;
+    while( i < closeButton.length){
+        console.log("loop");
+        const closeAsset = document.querySelectorAll(".close");
+        closeAsset.forEach(div => div.addEventListener('click', close));
+        i++;
+      
+    };
+
+    
+
+    function close(){
+        console.log("click delete");
+        let closeButtons = document.getElementById("asset-9").appendChild.length;
+        if(closeButtons) {
+            document.getElementsByClassName("removeAsset")[0].remove();
+        }
+
+        
+        
+    };
+
+});
+
+const plusBtn = document.querySelectorAll("plus_btn");
+//     console.log(plusBtn);
+//     plusBtn.forEach(span => span.addEventListener('click', createAsset));
+
+//     function createAsset(){
+//         console.log("click")
+//         let addAsset = document.getElementById("asset");
+//         let asset = document.createElement("div");
+        
+//         asset.innerHTML =
+//         ` 
+//            <select class= "assetBox" name="assetType" id="Type">
+//                <option value="Crypto">&#xf379;</option>
+//                <option value="Stock">&#xf201;</option>
+//                <option value="commodities">&#xf3a5;</option>
+//                <option value="bussiness">&#xf0b1;</option>
+//                <option value="fiat">&#xf19c;</option>
+//                <option value="real-estate">&#xe00d;</option>
+//            </select>
+//            <span>
+//                <input class="pInput" type="number" pattern="[0-9.,]+" placeholder="$0" name="amount" id="assets-el"/>
+//            </span> 
+//            `;
+    
+//         asset.classList.add("input_box", "inputBorder", "removeAsset");
+//         addAsset.appendChild(asset); 
--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,46 +1,155 @@
-// select each nav button
-const addButton = document.querySelectorAll(".plus");
-
-// add event listener to each button
-addButton.forEach(div => div.addEventListener('click', incrementAsset));
-
-const inputNum = document.querySelectorAll("#assets-el");
-console.log(inputNum)
-
-const closeAsset = document.querySelectorAll("#close");
-closeAsset.forEach(div => div.addEventListener('click', close));
-console.log(closeAsset)
-
-
-function incrementAsset() {
-
-    let addAsset = document.getElementById("add_asset");
-    let asset = document.createElement("div");
-    asset.innerHTML =
-    `   <select class= "assetBox" name="assetType" id="Type">
-            <option value="Crypto">&#xf379;</option>
-            <option value="Stock">&#xf201;</option>
-            <option value="commodities">&#xf3a5;</option>
-            <option value="bussiness">&#xf0b1;</option>
-            <option value="fiat">&#xf19c;</option>
-            <option value="real-estate">&#xe00d;</option>
-        </select>
-        <span>
-            <input class="pInput" type="number" pattern="[0-9.,]+" placeholder="$0" name="amount" id="assets-el"/>
-        </span> 
-        <div id="close" class="pSymbol">
-            <span>X</span>
+document.addEventListener('DOMContentLoaded', function(){ //DOM loads content with a running function to avoid latency and rendering time for the user
+    let addAsset = document.getElementById("asset"); // Selecting the div with an Id of "asset"
+ 
+    for(let i = 0; i < 10; i++){ // Running a for loop to input a unique id "${i}" for the HTML Content
+        addAsset.innerHTML = `  
+            <label class="input_label" for="asset-el">Assets</label>    
+            <div id="asset-${i}" class="input_box inputBorder removeAsset">
+                <select class= "assetBox" name="assetType" id="Type">
+                    <option value="Crypto">&#xf379;</option>
+                    <option value="Stock">&#xf201;</option>
+                    <option value="commodities">&#xf3a5;</option>
+                    <option value="bussiness">&#xf0b1;</option>
+                    <option value="fiat">&#xf19c;</option>
+                    <option value="real-estate">&#xe00d;</option>
+                </select>
+                <span>
+                    <input class="pInput" type="number" pattern="[0-9.,]+" placeholder="$0" name="amount" id="assets-el"/>
+                </span> 
+                <div class="pSymbol close">
+                    <span>X</span>
+                </div>
+            </div>
+        <div id="asset-${i}" class="input_box inputBorder removeAsset">
+            <select class= "assetBox" name="assetType" id="Type">
+                <option value="Crypto">&#xf379;</option>
+                <option value="Stock">&#xf201;</option>
+                <option value="commodities">&#xf3a5;</option>
+                <option value="bussiness">&#xf0b1;</option>
+                <option value="fiat">&#xf19c;</option>
+                <option value="real-estate">&#xe00d;</option>
+            </select>
+            <span>
+                <input class="pInput" type="number" pattern="[0-9.,]+" placeholder="$0" name="amount" id="assets-el"/>
+            </span> 
+            <div class="pSymbol close">
+                <span>X</span>
+            </div>
+        </div>
+        <div id="asset-${i}" class="input_box inputBorder removeAsset">
+            <select class= "assetBox" name="assetType" id="Type">
+                <option value="Crypto">&#xf379;</option>
+                <option value="Stock">&#xf201;</option>
+                <option value="commodities">&#xf3a5;</option>
+                <option value="bussiness">&#xf0b1;</option>
+                <option value="fiat">&#xf19c;</option>
+                <option value="real-estate">&#xe00d;</option>
+            </select>
+            <span>
+                <input class="pInput" type="number" pattern="[0-9.,]+" placeholder="$0" name="amount" id="assets-el"/>
+            </span> 
+            <div class="pSymbol close">
+                <span>X</span>
+            </div>
         </div>`;
+    };
 
-    asset.classList.add("input_box", "inputBorder");
-    addAsset.appendChild(asset);
-    console.log(asset);
-    console.log(addAsset);
+   // Add button
 
-};
-
-function close() {
+    let addButton = document.getElementById("plus_btn"); // Add button, function, which replicates the HTML when clicked
+    addButton.addEventListener('click', function(){
+        console.log("click add")
+        let addAsset = document.getElementById("asset");
+        let asset = document.createElement("div");
+        
+        asset.innerHTML =
+        ` 
+           <select class= "assetBox" name="assetType" id="Type">
+               <option value="Crypto">&#xf379;</option>
+               <option value="Stock">&#xf201;</option>
+               <option value="commodities">&#xf3a5;</option>
+               <option value="bussiness">&#xf0b1;</option>
+               <option value="fiat">&#xf19c;</option>
+               <option value="real-estate">&#xe00d;</option>
+           </select>
+           <span>
+               <input class="pInput" type="number" pattern="[0-9.,]+" placeholder="$0" name="amount" id="assets-el"/>
+           </span> 
+           `;
     
-    let remove =  document.getElementsByClassName("input_box", "inputBorder")[3].remove(); 
+        asset.classList.add("input_box", "inputBorder", "removeAsset"); // Add classes to the parent div for the main content
+        addAsset.appendChild(asset);
+        
+        let addSpan = document.createElement("div"); // Add the span close button separately in order to add targeted classes for the closeBtn function
+
+        addSpan.innerHTML = `<span>X</span>`;
+        addSpan.classList.add("pSymbol", "close");
+        asset.appendChild(addSpan);
+
+        for (let i = 0; i < 10; i++){
+            asset.setAttribute("id", `asset-${i}`); // For loop adds unique id "${i}" for the user created HTML Content
+        };
+
+        // Close button
+
+        const closeBtn = document.querySelectorAll(".close") // The querySelectorAll targets each class with the name "close"
+        closeBtn.forEach((btn) => {
+             btn.addEventListener('click', ()=>{
+                btn.parentElement.style.display = "none";
+                console.log("click delete");
+             });
+        });
+        
+    });
+
     
-};
+    const closeBtn = document.querySelectorAll(".close") // Repeated code block targeting the original HTML when the DOM loads on start
+    closeBtn.forEach((btn) => {
+         btn.addEventListener('click', ()=>{
+            btn.parentElement.style.display = "none";
+            console.log("click delete");
+         });
+    });
+
+
+    // document.getElementById("assets-el").addEventListener('keyup', addition);
+    
+    // function addition(){
+    //       console.log("adding");
+    //       let addTogether = document.getElementById("assets-el");
+    //       console.log(addTogether);
+    //  };
+
+  
+    
+    const netWorth = document.getElementById("net_worth").value;
+    let addTogether = document.querySelectorAll("#assets-el");
+     addTogether.forEach((input) => {
+          input.addEventListener('click', ()=>{
+            let sum = 0;
+            let num = document.getElementById("assets-el").value;
+            let numArray = [];
+            let size = 3;
+            console.log(numArray);
+
+
+            for (let i = 0; i < addTogether.length; i++){
+                numArray[i] = document.getElementById("assets-el").value + (i+1) ;
+            };
+        
+            console.log(sum);
+            console.log(numArray);
+
+          });
+     });
+
+   
+   
+});
+
+ // Portfolio calculations
+
+
+
+
+
