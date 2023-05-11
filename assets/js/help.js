bf728e01f7f8a93454608d938feea6b91b3182d9
Ry-F3 <rhysfeew@gmail.com>
2023-05-11 13:20:58 +0000

    Test - help.js file with jshint no major issues found

--- assets/js/help.js
+++ assets/js/help.js
@@ -1,10 +1,8 @@
 document.getElementById("help_icon").addEventListener('click', help);
 
 function help(){
-  let help = document.getElementById("help_icon");
-  console.log("click");
 
      let text = document.getElementById("text_display");
      text.style.display = "block";
 
-};
+}
