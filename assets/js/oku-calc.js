72d05adedc4d5ce4705681865c0867949c483a23
Ry-F3 <rhysfeew@gmail.com>
2023-06-22 17:30:47 +0000

    Fix oku-calc file js - call the unused variables and have the dom load content correctly. Restructure the functions and validate correct and incorrect inputs

--- assets/js/oku-calc.js
+++ assets/js/oku-calc.js
@@ -1,151 +1,274 @@
-document.getElementById("switch").addEventListener('click', calcSwitch); // Add event listeners for calculator switch function
+window.addEventListener('DOMContentLoaded', () => {
+    updateCalculation(); // Loads the calculation mode
+    barStart(); // Call barStart() after DOM content is loaded
+});
+
+// Add event listeners for calculator switch function
+document.getElementById("switch").addEventListener('click', calcSwitch);
+
 let capSwitch = document.getElementById("capSwitch"); // Switching labels
-let priceSwitch = document.getElementById("priceSwitch"); //Switching labels
-let checkBox = document.getElementById("check"); // Checkbox to swtich labels
+let priceSwitch = document.getElementById("priceSwitch"); // Switching labels
+let checkBox = document.getElementById("check"); // Checkbox to switch labels
 
-
-let price = 0;
 let inputCap = 0;
 let inputSupply = 0;
+let calculationMode = "divide"; // Track the current calculation mode
 
-onload = (event) => {
-    calcDivide();
-};
 
-function calcDivide() { // Calculate price per token
-
-    //change name of function to division and remove top event listeners & add function for multiplication
-    document.getElementById("cap-el").addEventListener('keyup', calcDivide); // Add event listeners to the input boxes
-    document.getElementById("sup-el").addEventListener('keyup', calcDivide);
-    document.getElementById("cap-el").addEventListener('keydown', barStart);
-
-    let inputCap = document.getElementById("cap-el").value;
-    let inputSupply = document.getElementById("sup-el").value;
-    let price = inputCap / inputSupply;
-
-    if (inputCap && inputSupply && price === 0) {
-        console.log("No input");
-    } else if (inputCap && inputSupply > price) {
-
-        if (price > 1) {
-            document.getElementById("price").value = price.toFixed(2); // If price greater than 1 only display 2 "0"s
-        } else {
-            document.getElementById("price").value = price.toFixed(12); // If the price is less than 1 add aditional "0"s
-        }
-    } else if (inputCap || inputSupply === 0) {
-        document.getElementById("price").value = 0;
+document.getElementById("cap-el").addEventListener('input', function (event) {
+    formatInputValue(event);
+    updateProbability();
+    if (calculationMode === "divide") {
+        calcDivide();
+        barStart(); // Call barStart() after updating the calculation
     } else {
-        console.log("Please input values");
-    }
-
-    function barStart() {
-        
-        if (inputCap < 5){
-            let certain = document.getElementById("certain");
-            certain.style.display = "block";
-        } else if(inputCap > 5) {
-            document.getElementById("cap-el").addEventListener('keyup', probability);
-            certain.style.display = "none";
-            console.log("keyup");
-        } else {
-            console.log("continue");
-        }
-
-     }
-    
-
-   function probability() {
-    if (inputCap < 100000000) {
-        let likely = document.getElementById("likely");
-        likely.style.display = 'block';
-        console.log("likely");
-    } else if (inputCap >= 100000000) {
-        let likely = document.getElementById("likely");
-        likely.style.display = 'none';
-    } else {
-        console.log("continue");
-    }
-    
-    if (inputCap >= 100000000 && inputCap < 100000000000) {
-        let maybe = document.getElementById("maybe");
-        maybe.style.display = 'block';
-        console.log("maybe");
-    } else {
-        console.log("continue");
-    };
-
-    if (inputCap >= 100000000000) {
-        let unlikely = document.getElementById("unlikely");
-        unlikely.style.display = 'block';
-        maybe.style.display = 'none';
-        console.log("unlikely");
-    } else {
-        console.log("continue");
-    }
-
-    if (inputCap >= 100000000000000) {
-        let impossible = document.getElementById("impossible");
-        impossible.style.display = 'block';
-        unlikely.style.display = 'none';
-    } else {
-        console.log("continue");
-    }
-
-   function barReset() {
-  
-        likely.style.display = 'none';
-        maybe.style.display = 'none';
-        unlikely.style.display = 'none';
-   }
-
-  }
-    
- }
-
- 
-function calcMultiply() {
-
-
-    if (checkbox.checked === true) {
-
-        //change name of function to division and remove top event listeners & add function for multiplication
-        document.getElementById("cap-el").addEventListener('keyup', calcMultiply); // Add event listeners to the input boxes
-        document.getElementById("sup-el").addEventListener('keyup', calcMultiply);
-
-        let inputCap = document.getElementById("cap-el").value;
-        let inputSupply = document.getElementById("sup-el").value;
-        let price = inputCap * inputSupply;
-        document.getElementById("price").value = price;
-    } else {
-        return calcDivide();
-    }
-
-}
-
-
-const checkbox = document.getElementById('check');
-
-
-function calcSwitch() {
-
-    if (checkbox.checked) {
-        checkbox.checked = false;
-        capSwitch.innerHTML = "Market Cap"; // When unchecked revert back to the original html label
-        priceSwitch.innerHTML = "Price";
-        console.log("I am dividing");
-        valueReset();
-    } else {
-        checkbox.checked = true;
-        capSwitch.innerHTML = "Price"; // When clicked switch html label name for change in functionality
-        priceSwitch.innerHTML = "Market Cap";
-        console.log("I am multiplying");
-        valueReset();
         calcMultiply();
     }
+});
+
+document.getElementById("sup-el").addEventListener('input', function (event) {
+    formatInputValue(event);
+    updateProbability();
+    if (calculationMode === "divide") {
+        calcDivide();
+    } else {
+        calcMultiply();
+    }
+});
+
+document.getElementById("cap-el").addEventListener('input', formatInputValue);
+document.getElementById("sup-el").addEventListener('input', formatInputValue);
+
+function formatInputValue(event) {
+    const inputElement = event.target;
+    const inputValue = inputElement.value;
+
+    // Check if the input value is empty
+    if (!inputValue) {
+        updateCalculation();
+        return;
+    }
+
+    // Remove commas from the value
+    const valueWithoutCommas = inputValue.replace(/,/g, '');
+
+    // Parse the input value as a number
+    const parsedValue = parseFloat(valueWithoutCommas);
+
+    // Check if the parsed value is a valid number
+    if (isNaN(parsedValue)) {
+        updateCalculation();
+        return;
+    }
+
+    // Format the value with commas
+    const formattedValue = formatNumberWithCommas(parsedValue);
+
+    // Check the calculation mode and handle exception for "multiply"
+    if (calculationMode === "multiply") {
+        // Check if the value contains a dot
+        if (inputValue.includes('.')) {
+            inputElement.value = inputValue;
+        } else {
+            inputElement.value = formattedValue;
+        }
+    } else {
+        inputElement.value = formattedValue;
+    }
+
+    updateCalculation();
+    barStart();
 }
 
-function valueReset() { // Reset values of calculator back to default
 
+function formatNumberWithCommas(number) { // Format the inputed numbers into a string, improving number readerbility
+    const parts = number.toString().split(".");
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return parts.join(".");
+}
+
+function validateInput() { // Check the input box to see if there is a valid number
+    if (isNaN(inputCap) || isNaN(inputSupply)) {
+        document.getElementById("price").value = "0";
+        return false;
+    }
+    return true;
+}
+
+function calcDivide() {
+    inputCap = parseFloat(document.getElementById("cap-el").value.replace(/,/g, ''));
+    inputSupply = parseFloat(document.getElementById("sup-el").value.replace(/,/g, ''));
+
+    if (isNaN(inputCap) || isNaN(inputSupply)) {
+        document.getElementById("price").value = "0";
+    } else if (inputCap === 0 || inputSupply <= 0) {
+        document.getElementById("price").value = "0";
+    } else {
+        let price = inputCap / inputSupply;
+
+        if (price > 0.009) {
+            document.getElementById("price").value = formatNumberWithCommas(price.toFixed(2));
+        } else {
+            document.getElementById("price").value = formatNumberWithCommas(price.toFixed(12));
+        }
+    }
+
+    barStart();
+}
+
+function barStart() {
+    let inputValid = false;
+  
+    if (calculationMode === "multiply") {
+      // Check for multiply calculation
+      inputValid =
+        inputCap !== "" &&
+        inputSupply !== "" &&
+        !isNaN(inputCap) &&
+        !isNaN(inputSupply) &&
+        parseFloat(inputCap) > 0 &&
+        parseFloat(inputSupply) > 0;
+    } else if (calculationMode === "divide") {
+      // Check for divide calculation
+      inputValid =
+        inputCap !== "" &&
+        inputSupply !== "" &&
+        !isNaN(inputCap) &&
+        !isNaN(inputSupply) &&
+        parseFloat(inputCap) > 0 &&
+        (parseFloat(inputSupply) > 0 || parseFloat(price.value.replace(/,/g, '')) > 0);
+    }
+  
+    if (inputValid) {
+      // All values are present and valid
+      // Hide the element
+      let certain = document.getElementById("certain");
+      certain.style.display = "none";
+    } else {
+      // Any of the values is empty, not a number, or less than or equal to 0
+      // Show the element
+      let certain = document.getElementById("certain");
+      certain.style.display = "block";
+    }
+  }
+  
+function updateProbability() {
+    let likely = document.getElementById("likely");
+    let maybe = document.getElementById("maybe");
+    let unlikely = document.getElementById("unlikely");
+    let impossible = document.getElementById("impossible");
+
+    likely.style.display = 'none';
+    maybe.style.display = 'none';
+    unlikely.style.display = 'none';
+    impossible.style.display = 'none';
+
+    let inputValue;
+    console.log(inputValue)
+
+    if (calculationMode === "divide") {
+        inputValue = inputCap;
+    } else {
+        inputValue = parseFloat(document.getElementById("price").value.replace(/,/g, ''));
+    }
+
+    if (inputValue < 100000000) {
+        likely.style.display = 'block';
+        console.log(inputValue)
+    } else if (inputValue >= 100000000 && inputValue < 100000000000) {
+        maybe.style.display = 'block';
+        console.log(inputValue)
+    } else if (inputValue >= 100000000000) {
+        unlikely.style.display = 'block';
+        console.log(inputValue)
+    } else if (inputValue >= 100000000000000) {
+        impossible.style.display = 'block';
+        console.log(inputValue)
+    }
+}
+
+
+function calcMultiply() {
+    inputCap = parseFloat(document.getElementById("cap-el").value.replace(/,/g, ''));
+    inputSupply = parseFloat(document.getElementById("sup-el").value.replace(/,/g, ''));
+
+    let price = inputCap * inputSupply;
+    document.getElementById("price").value = formatNumberWithCommas(price.toFixed(0));
+
+    if (!validateInput()) {
+        return;
+    }
+    console.log(price)
+    barStart();
+}
+
+document.getElementById("cap-el").addEventListener('input', function () {
+    formatInputValue();
+    probability();
+    if (calculationMode === "divide") {
+        calcDivide();
+    } else {
+        calcMultiply();
+    }
+});
+
+document.getElementById("sup-el").addEventListener('input', function () {
+    formatInputValue();
+    probability();
+    if (calculationMode === "divide") {
+        calcDivide();
+    } else {
+        calcMultiply();
+    }
+});
+
+function updateCalculation() {
+    if (calculationMode === "divide") {
+        calcDivide();
+        document.getElementById("cap-el").removeEventListener('keyup', calcMultiply);
+        document.getElementById("sup-el").removeEventListener('keyup', calcMultiply);
+        document.getElementById("cap-el").addEventListener('keyup', calcDivide);
+        document.getElementById("sup-el").addEventListener('keyup', calcDivide);
+    } else {
+        calcMultiply();
+        document.getElementById("cap-el").removeEventListener('keyup', calcDivide);
+        document.getElementById("sup-el").removeEventListener('keyup', calcDivide);
+        document.getElementById("cap-el").addEventListener('keyup', calcMultiply);
+        document.getElementById("sup-el").addEventListener('keyup', calcMultiply);
+    }
+}
+
+function calcSwitch() {
+    const inputSupplyElement = document.getElementById("sup-el");
+
+    if (checkBox.checked) {
+        checkBox.checked = false;
+        capSwitch.innerHTML = "Market Cap";
+        priceSwitch.innerHTML = "Price";
+        calculationMode = "divide"; // Set calculation mode to divide
+        valueReset();
+        updateCalculation();
+        updateProbability(); // Call updateProbability after switching the calculation mode
+        document.getElementById("cap-el").addEventListener('keyup', calcDivide);
+        document.getElementById("sup-el").addEventListener('keyup', calcDivide);
+        inputSupplyElement.addEventListener("input", barStart);
+        barStart();
+    } else {
+        checkBox.checked = true;
+        capSwitch.innerHTML = "Price";
+        priceSwitch.innerHTML = "Market Cap";
+        calculationMode = "multiply"; // Set calculation mode to multiply
+        valueReset();
+        updateCalculation();
+        updateProbability(); // Call updateProbability after switching the calculation mode
+        document.getElementById("cap-el").addEventListener('keyup', calcMultiply);
+        document.getElementById("sup-el").addEventListener('keyup', calcMultiply);
+        inputSupplyElement.addEventListener("input", barStart);
+        barStart();
+    }
+}
+
+function valueReset() {
     let supEl = document.getElementById("sup-el");
     let capEl = document.getElementById("cap-el");
     let price = document.getElementById("price");
@@ -153,9 +276,7 @@ function valueReset() { // Reset values of calculator back to default
     supEl.value = "";
     capEl.value = "";
     price.value = "0";
-
 }
-
 
 
 
